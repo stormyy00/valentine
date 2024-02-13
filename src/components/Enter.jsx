b0da8e54f68c53d1cb3108ff257bad945ef24f32
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-02-12 19:45:30 -0800

    added database

--- src/components/Enter.jsx
+++ src/components/Enter.jsx
@@ -3,16 +3,57 @@ import { useState } from 'react';
 import Link from 'next/link';
 import sanrio from "../../public/sanrio.gif";
 import Image from "next/image";
+import { collection, addDoc } from 'firebase/firestore'; 
+import { db } from '@/utils/firebase';
 
-const Enter = ({ onSubmit }) => {
+const Enter = ({ onSubmit, onTextSubmit }) => {
   const [name, setName] = useState('');
 
   const handleChange = (event) => {
     setName(event.target.value);
   };
 
-  const handleSubmit = () => {
+  // const handleSubmit = () => {
+
+  // };
+
+  const handleSubmit = async () => {
     onSubmit(name);
+    if (name.trim() !== '') {
+      try {
+        // Add data to Firestore
+        const docRef = await addDoc(collection(db, 'names'), {
+          content: name,
+          timestamp: new Date()
+        });
+  
+        console.log('Data sent to Firestore:', { content: name, timestamp: new Date() });
+        console.log('Document reference:', docRef.id);
+
+        // Check if onTextSubmit is a function before calling it
+        if (typeof onTextSubmit === 'function') {
+          onTextSubmit(name, docRef.id); // Pass the text and the document ID
+        }
+     
+  
+        // Call the function to send and fetch data to/from Flask server
+        // console.log("TEST", text)
+        // const response = await fetch(`/api/fetch?text=${text}`, {
+        //   method: "POST",
+        //   body: text
+        // })
+
+        // const data = await response.json()
+        // onbiasScore(data.average_probabilities.probability.Biased)
+        // onBarScore(data.compound_mean)
+
+        // console.log(data)
+  
+        setName('');
+      } catch (error) {
+        console.error('Error adding document to Firestore: ', error);
+      }
+    }
   };
 
   return (
